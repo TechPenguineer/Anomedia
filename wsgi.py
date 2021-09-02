2402a75166ecc61726b423c2176b2b71268d3718
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-09-02 15:29:55 -0400

    added discover route

--- wsgi.py
+++ wsgi.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def WARNING_PAGE():
         return render_template("warning.html")
 
+@app.route("/discover")
+def DISCOVER_PAGE():
+        return render_template('discover.html')
+
+
 if __name__ == "__main__":
     app.run(port=2010)
